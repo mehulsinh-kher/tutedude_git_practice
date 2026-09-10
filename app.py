3f8b658f76032dcdba32adea3847a5a78ac14391
mehul <mehulkher777@gmail.com>
2026-09-10 19:39:43 +0530

    rollback to item id commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, render_template
 import json
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,6 +13,20 @@ def api():
     data = json.load(file)
     return data
 
+@app.route("/addtask", methods=["POST"])
+def tasks():
+  new_item = {
+        "itemname": request.form.get("itemname"),
+        "description": request.form.get("description")
+    }
+  with open("data.json", "r" ) as file:
+    data = json.load(file)
+
+  data.append(new_item)
+  with open("data.json", "w") as f:
+    json.dump(data, f, indent=4)
+
+  return redirect("/")
 
 if __name__ == "__main__":
   app.run(debug=True)
