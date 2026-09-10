bcdde230617cbc36f6ec2bcf7f80462f8a545d40
mehul <mehulkher777@gmail.com>
2026-09-10 16:17:38 +0530

    added add task button process on backend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import json
 
 app = Flask(__name__)
@@ -14,6 +14,21 @@ def api():
     data = json.load(file)
     return data
 
+@app.route("/addtask")
+def tasks():
+  new_item = {
+        "itemname": request.form.get("itemname"),
+        "description": request.form.get("description")
+    }
+  with open("data.json", "r" ) as file:
+    data = json.load(file)
+    data = []
+
+  data.append(new_item)
+  with open("data.json", "w") as f:
+    json.dump(data, f, indent=4)
+
+  return redirect("/")
 
 if __name__ == "__main__":
   app.run(debug=True)
